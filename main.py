b1b8ee9b482519cfde23e4200efb4924311f4613
Rin <pelikhovmn@yandex.ru>
2024-10-26 16:45:12 +0300

    Первая версия проекта Запрос координат города

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from opencage.geocoder import OpenCageGeocode
 
 
 def get_coordinates(city, key):
-    geocoder = OpenCageGeocode(key)
-    query = city
-    results = geocoder.geocode(query)
-    if results:
-        return results[0]['geometry']['lat'], results[0]['geometry']['lng']
-    else:
-        return "Город не найден"
+    try:
+        geocoder = OpenCageGeocode(key)
+        query = city
+        results = geocoder.geocode(query)
+        if results:
+            return results[0]['geometry']['lat'], results[0]['geometry']['lng']
+        else:
+            return "Город не найден"
+    except Exception as e:
+        return f"Возникла ошибка {e}"
 
 
 key = '9b90a8325e184c319cdfbca0fde66855'
-city = "Moscow"
+city = "Ryazan"
 coordinates = get_coordinates(city, key)
 print(f"Координаты гордо {city}: {coordinates}")
